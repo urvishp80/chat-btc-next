ecf6ba1ff22cca50d8c12f0209ff66de5da9833e
Emmanuel-Develops <itakpeemma@gmail.com>
2023-02-14 15:52:06 +0100

    fix: edge function deployment

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,52 +1,47 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse, PageConfig } from 'next'
-// import { log } from 'next-axiom'
+import type { PageConfig } from 'next'
 
-// type Data = {
-//   [key]: string;
-// }
 
 export const config: PageConfig = {
-  runtime: "edge",
-}
-
-const reqConfig = {
-  "specification_hash": process.env.SPEC_HASH ?? "",
-  "config": {
-    "MODEL_SUMMARIZE": {
-      "provider_id":"openai",
-      "model_id":"text-davinci-002",
-      "use_cache":true
-    },
-    "ES_SEARCH": {
-      "use_cache":true
-    },
-    "MODEL_ANSWER_WITH_REFS": {
-      "provider_id":"openai",
-      "model_id":"text-davinci-002",
-      "use_cache":true
-    }
-  },
-  blocking: true,
+  runtime: "edge"
 }
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
+  req: Request,
+): Promise<Response> {
+  const { inputs } = (await req.json()) as {
+    inputs?: {question: string}[];
+  };
   const response = await fetch(process.env.BASE_URL ?? "", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.API_KEY ?? ""}`,
+      Authorization: `Bearer ${process.env.API_KEY}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      ...reqConfig,
-      inputs: req.body.inputs,
+      "specification_hash": process.env.SPEC_HASH,
+      "config": {
+        "MODEL_SUMMARIZE": {
+          "provider_id":"openai",
+          "model_id":"text-davinci-002",
+          "use_cache":true
+        },
+        "ES_SEARCH": {
+          "use_cache":true
+        },
+        "MODEL_ANSWER_WITH_REFS": {
+          "provider_id":"openai",
+          "model_id":"text-davinci-002",
+          "use_cache":true
+        }
+      },
+      blocking: true,
+      stream: false,
+      inputs
     }),
   });
 
   const data = await response.json();
-  console.log("Edge Function: ", {response, data})
-  res.status(200).json({ result: data })
+  return new Response(JSON.stringify({result: data}))
+  // res.status(200).json({ result: data })
 }
