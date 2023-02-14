d277d06cbf4596f8464d27972085f08b925ebc6a
Emmanuel-Develops <itakpeemma@gmail.com>
2023-02-14 13:40:17 +0100

    log edge function error

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse, PageConfig } from 'next'
-import { log } from 'next-axiom'
+// import { log } from 'next-axiom'
 
 // type Data = {
 //   [key]: string;
 // }
 
 export const config: PageConfig = {
-  runtime: "edge"
+  runtime: "edge",
+}
+
+const reqConfig = {
+  "specification_hash": process.env.SPEC_HASH ?? "",
+  "config": {
+    "MODEL_SUMMARIZE": {
+      "provider_id":"openai",
+      "model_id":"text-davinci-002",
+      "use_cache":true
+    },
+    "ES_SEARCH": {
+      "use_cache":true
+    },
+    "MODEL_ANSWER_WITH_REFS": {
+      "provider_id":"openai",
+      "model_id":"text-davinci-002",
+      "use_cache":true
+    }
+  },
+  blocking: true,
 }
 
 export default async function handler(
@@ -17,33 +37,16 @@ export default async function handler(
   const response = await fetch(process.env.BASE_URL ?? "", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
+      Authorization: `Bearer ${process.env.API_KEY ?? ""}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      "specification_hash": process.env.SPEC_HASH,
-      "config": {
-        "MODEL_SUMMARIZE": {
-          "provider_id":"openai",
-          "model_id":"text-davinci-002",
-          "use_cache":true
-        },
-        "ES_SEARCH": {
-          "use_cache":true
-        },
-        "MODEL_ANSWER_WITH_REFS": {
-          "provider_id":"openai",
-          "model_id":"text-davinci-002",
-          "use_cache":true
-        }
-      },
-      blocking: true,
+      ...reqConfig,
       inputs: req.body.inputs,
     }),
   });
 
   const data = await response.json();
-  log.info("Edge Function: ", {response, data})
-  console.log(data)
+  console.log("Edge Function: ", {response, data})
   res.status(200).json({ result: data })
 }
